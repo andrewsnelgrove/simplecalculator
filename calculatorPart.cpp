4610bc7b293a7d1267d8088384aec414c6d1524a
Andrew Snelgrove <45642597+andrewsnelgrove@users.noreply.github.com>
2019-09-24 22:02:37 -0230

    Beginning code on octal calculator portion.

---
+++ calculatorPart.cpp
@@ -0,0 +1,20 @@
+#include <isostream>
+using namespace std;
+
+int decimalToOctal(int decimalNumber){
+    int octalNumber;
+    int decimalNumberSplit [0] = {};
+    
+    //Split decimal number into digits
+    
+    
+    return octalNumber;
+    }
+
+int main(){
+    
+    #Octal Calcul
+    
+    
+    return 0;
+    }
